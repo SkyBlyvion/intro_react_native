0ab4de9b8ae7004161a31a3c49d27c4aeb67defd
Lopez Nicolas <richtofen60@gmail.com>
2024-03-19 13:42:47 +0100

    create structure

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const Account = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Account</Text>
     </View>
   )
 }
 
-export default App
+export default Account
--- src/screens/Note.jsx
+++ src/screens/Note.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const Note = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Note</Text>
     </View>
   )
 }
 
-export default App
+export default Note
